2be6ebaad8ae6e22bd1a10702932b4a416c90d91
HyeonWooGa <yeonwoopark22@gmail.com>
2022-08-31 23:57:23 +0900

    💻 Subscribe

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 // 리듀서 : 함수 상태의 Modifier == Setter, set 함수
 // state = 0 : 인자 state 값이 undefined 이면 0 할당, 초기화
 const countModifier = (count = 0, action) => {
@@ -17,16 +19,8 @@ const countModifier = (count = 0, action) => {
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
 
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
+countStore.subscribe(() => (number.innerText = countStore.getState()));
 
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
-
-countStore.dispatch({ type: "MINUS" });
-console.log(countStore.getState());
-
-countStore.dispatch({ type: "ADD" });
-console.log(countStore.getState());
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
